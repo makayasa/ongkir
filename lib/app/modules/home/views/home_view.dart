62e5197d568deca5363205d2d0717173ce150580
makayasa <makayasa@ozip.co.id>
2021-10-08 08:07:23 +0700

    ganti text apbar

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -12,7 +12,7 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomeView'),
+        title: Text('Test Changed From Mac'),
         centerTitle: true,
       ),
       body: ListView(
